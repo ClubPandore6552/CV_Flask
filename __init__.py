1630a20d137ce4a3b7159caf999d38db2fdabe28
ClubPandore6552 <149469055+ClubPandore6552@users.noreply.github.com>
2024-02-20 15:02:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
 from flask import Flask,render_template
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import Flask, render_template, request, redirect
+from flask import json
+from urllib.request import urlopen
+import sqlite3
 
 app = Flask(__name__) #creating flask app name
 
@@ -18,14 +23,20 @@ def resume_2():
 def resume_template():
     return render_template("resume_template.html")
 
+# Création d'une nouvelle route pour la lecture de la BDD
+@app.route("/consultation/")
+def ReadBDD():
+    conn = sqlite3.connect('/home/jarry/database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM livres;')
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 if(__name__ == "__main__"):
     app.run()
-    
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import Flask, render_template, request, redirect
-from flask import json
-from urllib.request import urlopen
-import sqlite3
 
 # Création d'une nouvelle route pour la lecture de la BDD
 @app.route("/consultation/")
